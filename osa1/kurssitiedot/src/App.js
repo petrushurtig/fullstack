24a81c904e88212ae5ceb48cc1c46a64ba350ffe
Petrus Hurtig <petrushurtig@hotmail.com>
2022-03-25 14:21:48 +0200

    moved consts outside of const App

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,3 +1,34 @@
+const Header = (props) => {
+  return(
+    <>
+      <h1>{props.course.name}</h1>
+    </>
+  )
+}
+const Content = (props) => {
+  return(
+    <>
+    <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
+    <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
+    <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
+    </>
+  )
+}
+const Part = (props) => {
+  return(
+    <>
+      <p>{props.part} {props.exercise}</p>
+    </>
+  )
+}
+const Total = (props) => {
+  return(
+    <>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </>
+  )
+}
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -15,37 +46,6 @@ const App = () => {
         exercises: 14
       }
     ]
-  }
-
-  const Header = (props) => {
-    return(
-      <>
-        <h1>{props.course.name}</h1>
-      </>
-    )
-  }
-  const Content = (props) => {
-    return(
-      <>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
-      </>
-    )
-  }
-  const Part = (props) => {
-    return(
-      <>
-        <p>{props.part} {props.exercise}</p>
-      </>
-    )
-  }
-  const Total = (props) => {
-    return(
-      <>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-      </>
-    )
   }
 
   return (
