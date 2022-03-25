ea801febe70ab9feddc7889138851a7172c20c61
Petrus Hurtig <petrushurtig@hotmail.com>
2022-03-25 15:07:09 +0200

    t1.7.: unicafe step2

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -14,15 +14,44 @@ const Button = (props) => {
     <button onClick={props.handleClick}>{props.name}</button>
   )
 }
+const Average = (props) => {
+  const ave = props.average / props.all;
+  return(
+    <>
+    average {ave}
+    </>
+  )
+}
+const Positive = (props) => {
+  const pos = (props.good / props.all) * 100;
+  return(
+    <div>
+    positive {pos} %
+    </div>
+  )
+}
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allVotes, setAllVotes] = useState(0);
+  const [average, setAverage] = useState(0);
 
-  const increaseGood = () => setGood(good +1);
-  const increaseNeutral = () => setNeutral(neutral +1);
-  const increaseBad = () => setBad(bad +1);
+  const increaseGood = () => { 
+    setGood(good +1);
+    setAllVotes(allVotes + 1);
+    setAverage(average +1);
+  }
+  const increaseNeutral = () => {
+    setNeutral(neutral +1);
+    setAllVotes(allVotes + 1);
+  }
+  const increaseBad = () => {
+    setBad(bad +1);
+    setAllVotes(allVotes + 1);
+    setAverage(average -1);
+  }
 
   return (
     <div>
@@ -34,6 +63,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {allVotes}</p>
+      <Average average={average} all={allVotes} />
+      <Positive good={good} all={allVotes} />
     </div>
   )
 }
