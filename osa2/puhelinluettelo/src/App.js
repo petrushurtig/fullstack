548621aa1d0c4751da561ca8440951809ec7ea64
Petrus Hurtig <petrushurtig@hotmail.com>
2022-03-30 16:19:20 +0300

    t.2.9.: puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '+313131-1313' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searched, setSearched] = useState('');
 
   const addPerson = (e) => {
     e.preventDefault();
@@ -29,11 +33,18 @@ const App = () => {
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value);
   }
+  const handleSearch = (e) => {
+    setSearched(e.target.value);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searched} onChange={handleSearch}/>
+      </div>
       <form onSubmit={addPerson}>
+        <h2>add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
@@ -45,8 +56,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => 
-        <p key={person.name}>{person.name} {person.number}</p>)}
+        {persons.filter(person => person.name.match(new RegExp(searched, "i")))
+        .map(person => 
+          <p key={person.name}>{person.name} {person.number}</p>
+      )}
     </div>
   )
 
